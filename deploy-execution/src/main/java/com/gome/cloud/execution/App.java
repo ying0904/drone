596aff064c219b6cef1dddc1e4a860751624f3db
blaiu <blaiu@163.com>
2015-11-23 22:15:46 +0800

    init

---
+++ deploy-execution/src/main/java/com/gome/cloud/execution/App.java
@@ -0,0 +1,13 @@
+package com.gome.cloud.execution;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
