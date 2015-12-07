673281153d4ea4778659718288a789ad9cc2f247
blaiu <blaiu@163.com>
2015-12-07 15:02:05 +0800

    update pom

---
+++ deploy-service/src/main/java/com/gome/cloud/restful/AppSystemRestful.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.gome.cloud.restful;
+
+import javax.ws.rs.client.Client;
+
+/**
+ * @author blaiu
+ *
+ */
+public class AppSystemRestful {
+
+//	private Client client;
+	
+//	public T getProject<T>(String uri) {
+//		Client client = ClientBuilder.newClient();
+//		client.register(JacksonFeature.class);
+//		WebTarget target = client.target(uri);
+//		Response response = target.request().get();
+//		if (null != response && response.getStatus() == 200) {
+//			response.readEntity(T.class)
+//		}
+//	}
+	
+	
+//	public static void main(String[] args) {
+//		
+//		Client client = ClientBuilder.newClient();
+//		client.register(JacksonFeature.class);
+//		String uri = String.format(endpointUrl + "%1$s", "getconditionset/" + namespaces + "/" + rcName);
+//		WebTarget target = client.target(uri);
+//		Response response = target.request().get();
+//		System.out.println(response.getMetadata().toString());
+//		getOK("getconditionset", "", response);
+//		return response.readEntity(ConditionDo.class);
+//	}
+	
+}
